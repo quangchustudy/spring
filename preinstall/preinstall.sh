030fd31fbbf1a41f86396a31a3f850051a2fd183
quangchu <quangchu@amazon.com.vn>
2023-03-29 18:21:38 +0700

    update preinstall.sh script

---
+++ preinstall/preinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+image_name="adoptopenjdk"
+image_tag="16_36-jre-hotspot"
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+if curl --head --silent --fail "https://public.ecr.aws/u8r7l3u8/adoptopenjdk:16_36-jre-hotspot"; then
+    echo "Image exists on Docker Hub: $image_name:$image_tag"
+else
+    echo "Image does not exist on Docker Hub: $image_name:$image_tag"
+    docker pull adoptopenjdk:16_36-jre-hotspot
+    docker tag $image_name:$image_tag public.ecr.aws/u8r7l3u8/$image_name:$image_tag
+    docker push public.ecr.aws/u8r7l3u8/$image_name:$image_tag
+    echo "Image uploaded to Docker Hub: $image_name:$image_tag"
+fi
