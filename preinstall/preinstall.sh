070c4776c4a01e8c763d4a1901f502f2cb930aa9
quangchu <quangchu@amazon.com.vn>
2023-03-29 18:52:52 +0700

    check login in preinstall.sh

--- preinstall/preinstall.sh
+++ preinstall/preinstall.sh
@@ -2,7 +2,8 @@
 
 image_name="adoptopenjdk"
 image_tag="16_36-jre-hotspot"
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+#if aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws;
+$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
 if curl --head --silent --fail "https://public.ecr.aws/u8r7l3u8/adoptopenjdk:16_36-jre-hotspot"; then
     echo "Image exists on Docker Hub: $image_name:$image_tag"
 else
